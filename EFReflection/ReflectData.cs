aab8071e67c9a639f1290ece297a5404ae107f6e
AR <Wizardskills@gmail.com>
2018-10-03 00:02:56 +0100

    Initial Commit

---
+++ EFReflection/ReflectData.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.CSharp.RuntimeBinder;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFReflection
+{
+    public static class ReflectData
+    {
+        public static DataTable GetData(string entitiesNamespace, string dataEntity, IEnumerable<string> columns)
+        {
+            DataTable returnDT = new DataTable();
+
+            Assembly assembly = Assembly.GetCallingAssembly();
+            Type entType = assembly.GetType(entitiesNamespace);
+
+            object ent = Activator.CreateInstance(entType);
+
+            PropertyInfo tablePropInfo = entType.GetProperty(dataEntity);
+
+            dynamic dataEntityValue = tablePropInfo.GetValue(ent, null);
+
+            IEnumerable<dynamic> dataEntityValues = ((IEnumerable)dataEntityValue).Cast<dynamic>();
+
+            foreach (string col in columns)
+            {
+                returnDT.Columns.Add(col);
+            }
+
+            foreach(dynamic row in dataEntityValues)
+            {
+                DataRow dr = returnDT.NewRow();
+
+                foreach (string col in columns)
+                {
+                    dr[col] = GetDynamicMember(row, col);
+                }
+
+                returnDT.Rows.Add(dr);
+            }
+
+            return returnDT;
+        }
+
+        private static string GetDynamicMember(object obj, string memberName)
+        {
+            var binder = Microsoft.CSharp.RuntimeBinder.Binder.GetMember(CSharpBinderFlags.None,
+                                                                         memberName,
+                                                                         obj.GetType(),
+                                                                         new[] {
+                                                                             CSharpArgumentInfo.Create(CSharpArgumentInfoFlags.None, null)
+                                                                         });
+
+            var callsite = CallSite<Func<CallSite, object, object>>.Create(binder);
+
+            return callsite.Target(callsite, obj).ToString();
+        }
+    }
+}
---
+++ EFReflectionDemo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using EFReflectionDemo.Model;
+
+namespace EFReflectionDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string outputFormat = "ID:{0}, Name:{1}, DOB:{2}";
+
+            using (ReflectDemoEntities ent = new Model.ReflectDemoEntities())
+            {
+                foreach (T_Person p in ent.T_Person)
+                {
+                    Console.WriteLine(string.Format(outputFormat, p.ID, p.Name, p.DOB.ToShortDateString()));
+                }
+            }
+
+            DataTable dt = EFReflection.ReflectData.GetData("EFReflectionDemo.Model.ReflectDemoEntities", "T_Person", new List<string>() { "ID", "Name", "DOB" });
+
+            foreach (DataRow dr in dt.Rows)
+            {
+                Console.WriteLine(string.Format(outputFormat, dr["ID"], dr["Name"], dr["DOB"]));
+            }
+        }
+    }
+}
